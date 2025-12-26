7fdd984ff87f8142b55b7e4749605792def27e26
MalakAdel2012 <mlkadlanwsany@gmail.com>
2025-12-26 13:59:25 -0800

    add project file

---
+++ javascript/1task.js
@@ -0,0 +1,78 @@
+let a="20";
+let b="15";
+let c="10";
+let d="5";
+
+let z=a+b;
+console.log("a+b="+z);
+document.write("a+b="+z);
+let s=a+c;
+console.log("a+c="+s);
+document.write("a+c="+s);
+let r=a+d;
+console.log("a+d="+r);
+document.write("a+d="+r);
+let l=b+c;
+console.log("b+c="+l);
+document.write("b+c="+l);
+let n=b+d;
+console.log("b+d="+n);
+document.write("b+d="+n);
+let m=c+d;
+console.log("c+d="+m);
+document.write("c+d="+m);
+
+let y=a-b;
+console.log("a-b="+y);
+document.write("a-b="+y);
+let v=a-c;
+console.log("a-c="+v);
+document.write("a-c="+v);
+let t=a-d;
+console.log("a-b="+t);
+document.write("a-b="+t);
+let u=b-c;
+console.log("b-c="+u);
+document.write("b-c="+u);
+let k=b-d;
+console.log("b-d="+k);
+document.write("b-d="+k);
+let i=c-d;
+console.log("c-d="+i);
+document.write("c-d="+i);
+
+let g=b*a;
+console.log("b*a="+g);
+document.write("b*a="+g);
+let p=c*a;
+console.log("c*a="+p);
+document.write("c*a="+p);
+let A=d*a;
+console.log("d*a="+A);
+document.write("d*a="+A);
+let q=b*c;
+console.log("b*c="+q);
+document.write("b*c="+q);
+let R=d*b;
+console.log("d*b="+R);
+document.write("d*b="+R);
+
+
+let S =b/a;
+console.log("b/a="+S);
+document.write("b/a="+S);
+let D=c/a;
+console.log("c/a="+D);
+document.write("c/a="+D);
+
+let F=a/d;
+console.log("a/d="+F);
+document.write("a/d="+F);
+let E=b/c;
+console.log("b/c="+E);
+document.write("b/c="+E);
+
+let Z=d/b;
+console.log("d/b="+Z);
+document.write
+let I=(20+15+10+5)
